e1cec92237f6b9cbb908d0bafca94f03d8141acd
Sander <biocoder-xbmc@xs4all.nl>
2026-02-22 20:54:29 +0100

    strong typed functions for getting the column value(s)

--- LogCenterDbRewrite/Extensions.cs
+++ LogCenterDbRewrite/Extensions.cs
@@ -6,19 +6,11 @@ namespace Sqlite.Synology.LogCenter
     internal static class Extensions
     {
         private static readonly DateTime epochStart = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-        public static object GetValue(this SqliteDataReader reader, logs field)
-        {
-            return reader.GetValue((int)field);
-        }
+        public static S GetValue<S>(this SqliteDataReader reader, logs field) where S : struct => (S)reader.GetValue((int)field);
+        public static string GetText(this SqliteDataReader reader, logs field) => (string)reader.GetValue((int)field);
 
-        internal static DateTime FromUnixInteger(this DateTime @his, long value)
-        {
-            return epochStart.AddSeconds(value);
-        }
-        internal static long ToUnixInteger(this DateTime @this)
-        {
-            return Convert.ToInt64((@this.ToUniversalTime() - epochStart).TotalSeconds);
-        }
+        internal static DateTime FromUnixInteger(this DateTime @his, long value) => epochStart.AddSeconds(value);
+        internal static long ToUnixInteger(this DateTime @this) => Convert.ToInt64((@this.ToUniversalTime() - epochStart).TotalSeconds);
 
     }
 }
